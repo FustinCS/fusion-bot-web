fe4159690882b51c2df63e0d899583464831ce92
FustinCS <FustinCS@gmail.com>
2024-09-12 19:13:30 -0700

    Remove unused variable

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/components/ui/custom/navbar";
 import { useSession } from "next-auth/react";
 
 export default function Home() {
-  const { data: session, status } = useSession();
+  const { status } = useSession();
   if (status === "loading") {
     return <></>;
   };
